ef384b7813edf0d2e03ba6ca9ca1ba50fd806cec
BlueDev <programmerbluedev@gmail.com>
2024-09-27 12:32:04 -0500

    updated DataGenerator class from datageneration class

--- autosynth/DataGenerator.py
+++ autosynth/DataGenerator.py
@@ -1,7 +1,4 @@
-import numpy as np
-import pandas as pd
-
-def generate_synthetic_data(n_samples=100, n_features=5):
-    X = np.random.rand(n_samples, n_features)
-    Y = np.random.choice([0, 1], size=n_samples)
-    return pd.DataFrame(X), Y
+#base class
+class DataGenerator:
+    def generate(self, **kwargs):
+        raise NotImplementedError("Subclasses must implement this method")
--- autosynth/datageneration.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def generate_synthetic_data(n_samples=100, n_features=5):
-    X = np.random.rand(n_samples, n_features)
-    Y = np.random.choice([0, 1], size=n_samples)
-    return pd.DataFrame(X), Y
